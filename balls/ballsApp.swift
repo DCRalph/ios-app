06987c21569ab4d9de10ac8282acfd3344c87cf6
Technoblade <william@williamgiles.co.nz>
2024-09-03 22:39:18 +1200

    Initial Commit

---
+++ balls/ballsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ballsApp.swift
+//  balls
+//
+//  Created by William Giles on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ballsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ballsTests/ballsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ballsTests.swift
+//  ballsTests
+//
+//  Created by William Giles on 03/09/2024.
+//
+
+import Testing
+@testable import balls
+
+struct ballsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
